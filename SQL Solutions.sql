de784a198eb4fd5c724baa817903d116616be26d
Collins <collinsmensah4478@outlook.com>
2025-09-04 15:18:46 -0400

    Update SQL Solutions.sql

--- SQL Solutions.sql
+++ SQL Solutions.sql
@@ -1,42 +1,42 @@
 ## Analysis Questions
-# 1. How many cars are in the dataset?
+ 1. How many cars are in the dataset?
 		SELECT COUNT(*) AS Total_Cars
         FROM Car_Data;
 	
-# 2. How many unique car models are in the dataset?
+ 2. How many unique car models are in the dataset?
 		SELECT COUNT(DISTINCT Name) AS Total_Number_Of_Diff_Cars
 		FROM Car_Data;
         
-# 3. Identify the top 5 most expensive cars in the dataset
+ 3. Identify the top 5 most expensive cars in the dataset
 		SELECT DISTINCT Name, Selling_Price
         FROM Car_Data
         ORDER BY Selling_Price DESC
         LIMIT 5;
         
-# 4.Identify the top 5 cheapest cars in the dataset.
+ 4.Identify the top 5 cheapest cars in the dataset.
 		SELECT DISTINCT Name, Selling_Price
         FROM Car_Data
         ORDER BY Selling_Price ASC
         LIMIT 5;
         
-# 5. What are the top 5 cars with the highest kilometers driven?
+ 5. What are the top 5 cars with the highest kilometers driven?
 		SELECT DISTINCT NAME, KM_Driven FROM CAR_DATA
         ORDER BY KM_Driven DESC
         LIMIT 5;
         
-# 6. How do average kilometers driven compare across fuel types?
+ 6. How do average kilometers driven compare across fuel types?
 		SELECT Fuel_Type, ROUND(AVG(KM_Driven),2) AS AVG_KG_Driven, COUNT(*) Total_Cars 
         FROM CAR_DATA
         GROUP BY Fuel_Type
         ORDER BY AVG_KG_Driven DESC;
 
-# 7. How many cars are there for each owner type (including test drive cars)?
+ 7. How many cars are there for each owner type (including test drive cars)?
 		SELECT Owner, COUNT(*) Total_Cars
         FROM Car_Data
         GROUP BY Owner
         ORDER BY Total_Cars DESC;
         
-# 8. Which Manufacturing Decade has the most cars in the dataset?
+ 8. Which Manufacturing Decade has the most cars in the dataset?
 		Select Model_Range_Year, Count(*) AS Number_Of_Cars 
         FROM 
 				( 
@@ -52,7 +52,7 @@
         ORDER BY Number_Of_Cars DESC;
         
         
-# 9. What is the average selling price for each seller type?
+ 9. What is the average selling price for each seller type?
 		SELECT	Seller_Type,
 				Count(*) AS Total_Cars,
 				ROUND(AVG(Selling_Price),2) AS Avg_Selling_Price
@@ -60,7 +60,7 @@
 		GROUP BY Seller_Type
         ORDER BY Avg_Selling_Price DESC;
         
-# 10. What is the average selling price for each fuel type?
+ 10. What is the average selling price for each fuel type?
 		SELECT	Fuel_Type,
 					Count(*) AS Total_Cars,
 					ROUND(AVG(Selling_Price),2) AS Avg_Selling_Price
@@ -68,7 +68,7 @@
 		GROUP BY Fuel_Type
 		ORDER BY Avg_Selling_Price DESC;
         
-# 11. Which transmisison type is more common in the data set?
+ 11. Which transmisison type is more common in the data set?
 		SELECT Transmission, Count(*) Total_Cars 
         FROM Car_Data
         GROUP BY Transmission
